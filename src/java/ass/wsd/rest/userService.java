34cfae1bb8a2675a8cce5f525ce9af1303da768f
Kieran O'Brien <krillino64@gmail.com>
2017-05-27 14:26:56 +1000

    added stuff

--- src/java/ass/wsd/rest/userService.java
+++ src/java/ass/wsd/rest/userService.java
@@ -4,9 +4,13 @@
  * and open the template in the editor.
  */
 package ass.wsd.rest;
-import ass.wsd.*;
+
+import javax.servlet.ServletContext;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+import javax.xml.bind.JAXBException;
+import java.io.*;
+import ass.wsd.*;
 
 /**
  *
@@ -14,6 +18,29 @@ import javax.ws.rs.core.*;
  */
 @Path("/users")
 public class userService {
+    
+    @Context
+    private ServletContext application;
+    
+    private UsersApp getUsersApp() throws JAXBException, IOException, Exception {
+        // The web server can handle requests from different clients in parallel.
+        // These are called "threads".
+        //
+        // We do NOT want other threads to manipulate the application object at the same
+        // time that we are manipulating it, otherwise bad things could happen.
+        //
+        // The "synchronized" keyword is used to lock the application object while
+        // we're manpulating it.
+        synchronized (application) {
+            UsersApp userApp = (UsersApp) application.getAttribute("userApp");
+            if (userApp == null) {
+                userApp = new UsersApp();
+                userApp.setFilePath(application.getRealPath("WEB-INF/users.xml"));
+                application.setAttribute("userApp", userApp);
+            }
+            return userApp;
+        }
+    }
 
     @Path("hello")
     @GET
@@ -25,7 +52,32 @@ public class userService {
     @Path("getInfo")
     @GET
     @Produces(MediaType.APPLICATION_XML)
-    public User findUser() {
-        
+    public User findUser(@QueryParam("email") String email) throws JAXBException, IOException, Exception {
+        return getUsersApp().getUsers().getUser(email);
     }
+    
+    @Path("getUsers")
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public Users getUsers() throws IOException, Exception {
+        return getUsersApp().getUsers();
+    }
+    
+    /**
+    @Path("getBookings")
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public Bookings getBookings() throws IOException, Exception {
+        return getBookingsApp().getBookings();
+    }
+    **/
+    
+    /**
+    @Path("getFlights")
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public Flights getFlights() throws IOException, Exception {
+        return getFlightsApp().getFlights();
+    }
+    **/
 }
