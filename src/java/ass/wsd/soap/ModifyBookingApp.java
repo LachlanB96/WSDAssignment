a05fb2fd125be80dc7530e5deb1011ed9cb0c77c
Lachlan Brown <lachlan@lachlanb.com>
2017-06-08 05:55:06 +1000

    Added fully functional SOAP (needs input changing)

--- src/java/ass/wsd/soap/ModifyBookingApp.java
+++ src/java/ass/wsd/soap/ModifyBookingApp.java
@@ -6,9 +6,17 @@
 package ass.wsd.soap;
 
 import ass.wsd.Booking;
+import ass.wsd.Bookings;
 import ass.wsd.BookingsApp;
+import ass.wsd.Flight;
+import ass.wsd.Flights;
 import ass.wsd.FlightsApp;
+import ass.wsd.User;
+import ass.wsd.Users;
+import ass.wsd.UsersApp;
+import com.sun.javafx.scene.web.Debugger;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Resource;
@@ -29,12 +37,24 @@ public class ModifyBookingApp {
     @Resource
     private WebServiceContext context;
 
-    /**
-     * This is a sample web service operation
-     */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    @WebMethod
+    public User login(String username, String password
+    ) {
+
+        ServletContext application = (ServletContext) context.getMessageContext().get(MessageContext.SERVLET_CONTEXT);
+        UsersApp app = (UsersApp) application.getAttribute("UsersApp");
+        if (app == null) {
+            app = new UsersApp();
+            try {
+                app.setFilePath(application.getRealPath("WEB-INF/users.xml"));
+            } catch (Exception ex) {
+                Logger.getLogger(ModifyBookingApp.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            application.setAttribute("UsersApp", app);
+        }
+        Users users = app.getUsers();
+        User user = users.login(username, password);
+        return user;
     }
 
     @WebMethod
@@ -54,8 +74,7 @@ public class ModifyBookingApp {
         return app.getBookings().getList();
 
     }
-    
-    
+
     @WebMethod
     public FlightsApp getFlightsApp() {
 
@@ -73,4 +92,67 @@ public class ModifyBookingApp {
         return app;
 
     }
+
+    @WebMethod
+    public void addListing(int flightID, String departureDate, String returnDate, String price, int numofSeats, String origin, String destination, String description, String flightType) throws Exception {
+
+        ServletContext application = (ServletContext) context.getMessageContext().get(MessageContext.SERVLET_CONTEXT);
+        FlightsApp app = (FlightsApp) application.getAttribute("FlightsApp");
+        if (app == null) {
+            app = new FlightsApp();
+            try {
+                app.setFilePath(application.getRealPath("WEB-INF/flights.xml"));
+            } catch (Exception ex) {
+                Logger.getLogger(ModifyBookingApp.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            application.setAttribute("FlightsApp", app);
+        }
+        Flight newFlight = new Flight(flightID, departureDate, departureDate, price, numofSeats, origin, destination, description, flightType);
+
+        Flights existingFlights = app.getFlights();
+        existingFlights.addFlight(newFlight);
+        app.updateXML(existingFlights);
+    }
+
+    @WebMethod
+    public void addBooking(int userID, int flightID, int bookingID, String origin, String destination, String flightType, String departureDate, String returnDate, String description) throws Exception {
+
+        ServletContext application = (ServletContext) context.getMessageContext().get(MessageContext.SERVLET_CONTEXT);
+        BookingsApp app = (BookingsApp) application.getAttribute("BookingsApp");
+        if (app == null) {
+            app = new BookingsApp();
+            try {
+                app.setFilePath(application.getRealPath("WEB-INF/bookings.xml"));
+            } catch (Exception ex) {
+                Logger.getLogger(ModifyBookingApp.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            application.setAttribute("BookingsApp", app);
+        }
+        Booking newBooking = new Booking(userID, flightID, bookingID, origin, destination, flightType, departureDate, returnDate, description);
+
+        Bookings existingBookings = app.getBookings();
+        existingBookings.addBooking(newBooking);
+        app.updateXML(existingBookings);
+    }
+
+    @WebMethod
+    public void closeListing(int listingID) throws Exception {
+
+        ServletContext application = (ServletContext) context.getMessageContext().get(MessageContext.SERVLET_CONTEXT);
+        FlightsApp app = (FlightsApp) application.getAttribute("FlightsApp");
+        if (app == null) {
+            app = new FlightsApp();
+            try {
+                app.setFilePath(application.getRealPath("WEB-INF/flights.xml"));
+            } catch (Exception ex) {
+                Logger.getLogger(ModifyBookingApp.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            application.setAttribute("FlightsApp", app);
+        }
+        Flights existingFlights = app.getFlights();
+        Flight listingToRemove = existingFlights.getFlightID(listingID);
+        existingFlights.removeFlight(listingToRemove);
+        app.updateXML(existingFlights);
+    }
+
 }
--- src/java/ass/wsd/soap/client/ModifyBookingAppClient.java
+++ src/java/ass/wsd/soap/client/ModifyBookingAppClient.java
@@ -7,6 +7,7 @@ package ass.wsd.soap.client;
 
 import ass.wsd.Booking;
 import ass.wsd.Flight;
+import ass.wsd.User;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,43 +22,130 @@ import java.util.Scanner;
  */
 public class ModifyBookingAppClient {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, Exception_Exception {
 
-        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter a number to take action: ");
-        System.out.println("(1) print flights");
-        System.out.println("(2) create viewing");
-        System.out.println("(3) create booking");
-        System.out.println("(4) cancel booking");
-        System.out.print("==> ");
-        //String s = scan.next();
-        String input = scan.readLine();
         ModifyBookingApp_Service locator = new ModifyBookingApp_Service();
         ModifyBookingApp port = locator.getModifyBookingAppPort();
-        System.out.println(input);
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+        ass.wsd.soap.client.User user = null;
+        String userInput;
+        boolean running = true;
 
-        if (input.equals("1")) {
-            String userInput;
-            String userIDFilter = "342345";
-            String flightTypeFilter = "b";
-            String numSeatsFilter = "10";
-            System.out.println("PRINT FLIGHTS");
-            System.out.println("Search criteria (enter for default)");
-            System.out.print("Customer ID (342345): ");
-            userInput = scan.readLine();
-            if(!"".equals(userInput.trim())) userIDFilter = userInput;
-            System.out.print("(b)usiness or (e)conomical (b): ");
-            userInput = scan.readLine();
-            if(!"".equals(userInput.trim())) flightTypeFilter = userInput;
-            System.out.print("Number of Seats (10): ");
-            userInput = scan.readLine();
-            if(!"".equals(userInput.trim())) numSeatsFilter = userInput;
-            System.out.println("--------------------------------------");
-            FlightsApp flightsApp = port.getFlightsApp();
-            Flights flights = flightsApp.getFlights();
-            List<ass.wsd.soap.client.Flight> flightList = flights.getFlight();
-            for (int i = 0; i < flightList.size(); i++) {
-                //if (String.valueOf(flightList.get(i).id).equals(userIDFilter)) {
+        while (running) {
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("### MAIN MENU ###");
+            System.out.println();
+
+            if (user != null) {
+                System.out.println("Hello " + user.getName() + "!");
+            } else {
+                System.out.println("Welcome unknown user!");
+            }
+
+            System.out.println();
+            System.out.println("On prompts, leave the input empty to use the default value in brackets (<value>).");
+            System.out.println("Enter a number to take action: ");
+            System.out.println("(1) Login to account");
+            System.out.println("(2) Create a listing");
+            System.out.println("(3) Create a booking");
+            System.out.println("(4) View current bookings");
+            System.out.println("(5) View current listings");
+            System.out.println("(6) Close a listing");
+            System.out.println("(7) Exit application");
+            String input = userInputFormatted("==> ", "1", scan, false);
+
+            // LOGIN METHOD ###########################################################################
+            if (input.equals("1")) {
+
+                if (user != null) {
+                    System.out.println("You are already logged in as " + user.getName() + "!");
+                } else {
+                    String username = "r@r.r";
+                    String password = "r";
+
+                    System.out.println("Login to account.");
+                    System.out.println("Username (r@r.r): ");
+                    userInput = ""; //scan.readLine();
+                    if (!"".equals(userInput.trim())) {
+                        username = userInput;
+                    }
+                    System.out.println("Password (r): ");
+                    userInput = ""; //scan.readLine();
+                    if (!"".equals(userInput.trim())) {
+                        password = userInput;
+                    }
+                    System.out.println("Authenticating...");
+                    user = port.login(username, password);
+                    if (user != null) {
+                        System.out.println("Successfully logged in. Welcome " + user.getName() + "!");
+                    } else {
+                        System.out.println("Username and/or password does not exist. Exitting.");
+                    }
+
+                }
+            } // Create listing METHOD ###########################################################################
+            else if (input.equals("2")) {
+                System.out.println("Create a listing.");
+                int flightID = Integer.valueOf(userInputFormatted("Listing origin", "1007", scan, true));
+                String origin = userInputFormatted("Listing origin", "Sydney", scan, true);
+                String destination = userInputFormatted("Listing origin (Sydney): ", "Melbourne", scan, true);
+                int numofSeats = Integer.valueOf(userInputFormatted("Listing origin (Sydney): ", "8", scan, true));
+                String departureDate = userInputFormatted("Listing origin (Sydney): ", "2017-06-16", scan, true);
+                String flightType = userInputFormatted("Listing origin (Sydney): ", "Business", scan, true);
+                String price = userInputFormatted("Listing origin (Sydney): ", "100.00", scan, true);
+                String description = userInputFormatted("Listing origin (Sydney): ", "Business flight from Sydney to Melbourne.", scan, true);
+                port.addListing(flightID, departureDate, departureDate, price, numofSeats, origin, destination, description, flightType);
+            } else if (input.equals("3")) {
+                System.out.println("Create a booking.");
+                int userID = Integer.valueOf(userInputFormatted("Listing origin", "342345", scan, true));
+                int flightID = Integer.valueOf(userInputFormatted("Listing origin", "1007", scan, true));
+                int bookingID = Integer.valueOf(userInputFormatted("Listing origin", "103", scan, true));
+                String origin = userInputFormatted("Listing origin", "Sydney", scan, true);
+                String destination = userInputFormatted("Listing origin (Sydney): ", "Melbourne", scan, true);
+                String departureDate = userInputFormatted("Listing origin (Sydney): ", "2017-06-16", scan, true);
+                String flightType = userInputFormatted("Listing origin (Sydney): ", "Business", scan, true);
+                String description = userInputFormatted("Listing origin (Sydney): ", "Business flight from Sydney to Melbourne.", scan, true);
+                port.addBooking(userID, flightID, bookingID, origin, destination, flightType, departureDate, departureDate, description);
+            } // view listing METHOD ###########################################################################
+            else if (input.equals("4")) {
+                System.out.println("PRINT BOOKINGS");
+                System.out.println("Search criteria (enter for default)");
+                String userIDFilter = userInputFormatted("Customer ID", "342345", scan, true);
+                String flightTypeFilter = userInputFormatted("(b)usiness or (e)conomical", "b", scan, true);
+                String numSeatsFilter = userInputFormatted("Number of Seats", "10", scan, true);
+                System.out.println("--------------------------------------");
+                List<ass.wsd.soap.client.Booking> bookings = port.getBookingsXML();
+                for (int i = 0; i < bookings.size(); i++) {
+                    if (String.valueOf(bookings.get(i).id).equals(userIDFilter)) {
+                        out.print(bookings.get(i).bookingID);
+                        out.print(" | ");
+                        out.print(bookings.get(i).departureDate);
+                        out.print(" | ");
+                        out.print(bookings.get(i).description);
+                        out.print(" | ");
+                        out.print(bookings.get(i).destination);
+                        out.print(" | ");
+                        out.print(bookings.get(i).flightID);
+                        out.print(" | ");
+                        out.print(bookings.get(i).flightType);
+                        out.print(" | ");
+                        out.print(bookings.get(i).id);
+                        out.print(" | ");
+                        out.print(bookings.get(i).origin);
+                        out.print(" | ");
+                        out.println(bookings.get(i).returnDate);
+                    }
+                }
+            } else if (input.equals("5")) {
+                System.out.println("PRINT LISTINGS");
+                FlightsApp flightsApp = port.getFlightsApp();
+                Flights flights = flightsApp.getFlights();
+                List<ass.wsd.soap.client.Flight> flightList = flights.getFlight();
+                for (int i = 0; i < flightList.size(); i++) {
+                    //if (String.valueOf(flightList.get(i).id).equals(userIDFilter)) {
                     out.print(flightList.get(i).flightID);
                     out.print(" | ");
                     out.print(flightList.get(i).departureDate);
@@ -75,53 +163,30 @@ public class ModifyBookingAppClient {
                     out.print(flightList.get(i).description);
                     out.print(" | ");
                     out.println(flightList.get(i).flightType);
-                //}
-            }
-
-            //x.getBookings();
-        }
-        else if (input.equals("2")) {
-            String userInput;
-            String userIDFilter = "342345";
-            String flightTypeFilter = "b";
-            String numSeatsFilter = "10";
-            System.out.println("PRINT FLIGHTS");
-            System.out.println("Search criteria (enter for default)");
-            System.out.print("Customer ID (342345): ");
-            userInput = scan.readLine();
-            if(!"".equals(userInput.trim())) userIDFilter = userInput;
-            System.out.print("(b)usiness or (e)conomical (b): ");
-            userInput = scan.readLine();
-            if(!"".equals(userInput.trim())) flightTypeFilter = userInput;
-            System.out.print("Number of Seats (10): ");
-            userInput = scan.readLine();
-            if(!"".equals(userInput.trim())) numSeatsFilter = userInput;
-            System.out.println("--------------------------------------");
-            List<ass.wsd.soap.client.Booking> bookings = port.getBookingsXML();
-            for (int i = 0; i < bookings.size(); i++) {
-                if (String.valueOf(bookings.get(i).id).equals(userIDFilter)) {
-                    out.print(bookings.get(i).bookingID);
-                    out.print(" | ");
-                    out.print(bookings.get(i).departureDate);
-                    out.print(" | ");
-                    out.print(bookings.get(i).description);
-                    out.print(" | ");
-                    out.print(bookings.get(i).destination);
-                    out.print(" | ");
-                    out.print(bookings.get(i).flightID);
-                    out.print(" | ");
-                    out.print(bookings.get(i).flightType);
-                    out.print(" | ");
-                    out.print(bookings.get(i).id);
-                    out.print(" | ");
-                    out.print(bookings.get(i).origin);
-                    out.print(" | ");
-                    out.println(bookings.get(i).returnDate);
+                    //}
                 }
+            } else if (input.equals("6")) {
+                System.out.println("CLOSE LISTING");
+                int listingID = Integer.valueOf(userInputFormatted("Customer ID", "342345", scan, false));
+                port.closeListing(listingID);
+            } else if (input.equals("7")) {
+                running = false;
             }
-
-            //x.getBookings();
         }
     }
 
+    private static String userInputFormatted(String prompt, String defaultValue, BufferedReader scan, boolean isDebug) throws IOException {
+        System.out.print(prompt + " (" + defaultValue + "): ");
+        if (isDebug) {
+            return defaultValue;
+        } else {
+            String userInput = scan.readLine();
+            System.out.println();
+
+            if (!"".equals(userInput.trim())) {
+                return userInput;
+            }
+            return defaultValue;
+        }
+    }
 }
